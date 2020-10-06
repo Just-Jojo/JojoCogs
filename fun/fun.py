d0b650a7c7603b62d6db7058d2e47a0dfaf249e6
Jojo <jojohelp39@gmail.com>
2020-10-06 14:20:17 -0400

    Dev cog init

--- fun/fun.py
+++ fun/fun.py
@@ -10,15 +10,3 @@ class Fun(commands.Cog):
     @commands.command()
     async def coffee(self, ctx):
         await ctx.send("☕")
-
-    @commands.command(hidden=True)
-    @commands.is_owner()
-    async def sudo(self, ctx, user: Member, *, command):
-        """Sudo another user invoking a command.
-        The prefix must not be entered.
-        """
-        msg = copy(ctx.message)
-        msg.author = user
-        msg.content = ctx.prefix + command
-
-        ctx.bot.dispatch("message", msg)
