481613a495d94b285d1f1913ee4748af995f0374
Jojo <jojohelp39@gmail.com>
2020-10-13 17:24:04 -0400

    Account check

--- econotools/economy_tools.py
+++ econotools/economy_tools.py
@@ -7,11 +7,18 @@ class EconomyTools(commands.Cog):
         self.bot = bot
         self.config = Config.get_conf(self, 646613323, force_registration=True)
 
+    async def get_currency(self, ctx: commands.Context, user: dis.Member):
+        x = await bank.get_balance(user)
+        y = await bank.get_currency_name(ctx.guild)
+        return x, y
+
     @commands.command()
-    async def balance(self, ctx, user: dis.Member = None):
+    async def balance(self, ctx, user: dis.Member = None, account: bool = False):
         if user is None:
             user = ctx.author
-
-        user_bal = await bank.get_balance(user)
-        user_bank_currency_name = await bank.get_currency_name(ctx.guild)
-        await ctx.send("{0.display_name} has {1} {2}".format(user, user_bal, user_bank_currency_name))
+        if account is False:
+            user_bal, user_bank_cur_name = await self.get_currency(ctx, user)
+            await ctx.send("{0.display_name} has {1} {2}".format(user, user_bal, user_bank_cur_name))
+        else:
+            acc = await bank.get_account(user)
+            await ctx.send(acc)
