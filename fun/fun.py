0f6cbee182a0ef808ee26237fe55a256ae165e2c
Jojo <jojohelp39@gmail.com>
2020-10-13 18:19:43 -0400

    Better coffee command (actually a store command :p

--- fun/fun.py
+++ fun/fun.py
@@ -1,4 +1,4 @@
-from redbot.core import commands
+from redbot.core import commands, bank
 from discord import Member
 from copy import copy
 
@@ -7,6 +7,27 @@ class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def coffee(self, ctx):
-        await ctx.send("☕")
+    def readable_dict(self, dictionary: dict):
+        x = []
+        for key, item in dictionary.items():
+            y = "{0}: {1}".format(key, item)
+            x.append(y)
+        return "\n".join(x)
+
+    @commands.group()
+    async def store(self, ctx):
+        """Store commmands"""
+
+    @store.command(name="buy")
+    async def _buy(self, ctx, item: str):
+        stalk = {
+            "coffee": 15,
+            "doughnut": 10,
+            "scone": 20
+        }
+        if item and item.lower() in stalk.keys():
+            if await bank.can_spend(ctx.author, amount=stalk[item.lower()]):
+                cur_name = await bank.get_currency_name(ctx.guild)
+                await ctx.send("You bought a {name} for {item} {currency_name}".format(name=item.lower(), item=stalk[item.lower()]), currency_name=cur_name)
+        else:
+            await ctx.send(self.readable_dict(stalk))
