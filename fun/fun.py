0b3c657fe707d336563a9014d5e4562afbf0b926
Jojo <jojohelp39@gmail.com>
2020-10-13 20:54:52 -0400

    This *should* work

--- fun/fun.py
+++ fun/fun.py
@@ -7,14 +7,12 @@ class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(self, 13814755994)
-
-        self.config.register_member(
-            items={
-                "coffee": 0,
-                "dougnut": 0,
-                "scone": 0
-            }
-        )  # **_default_user)
+        self.config.register_global(
+            coffee=10,
+            scone=15,
+            doughnut=9
+        )
+        self.config.register_member(items={})
 
     def readable_dict(self, dictionary: dict):
         x = []
@@ -29,20 +27,17 @@ class Fun(commands.Cog):
 
     @store.command(name="buy")
     async def _buy(self, ctx, item: str):
-        stalk = {
-            "coffee": 15,
-            "doughnut": 10,
-            "scone": 20
-        }
-        if item and item.lower() in stalk.keys():
-            if await bank.can_spend(ctx.author, amount=stalk[item.lower()]):
-                cur_name = await bank.get_currency_name(ctx.guild)
-                up = await self.config.user(ctx.author).items.get_raw(item.lower())
-                upd = up + 1
-                await self.config.user(ctx.author).items.set_raw(item.lower(), upd)
-                await ctx.send("You bought a {0} for {1} {2}".format(item.lower(), stalk[item.lower()], cur_name))
-        else:
-            await ctx.send(self.readable_dict(stalk))
+        try:
+            cost = await self.config.user(ctx.author).items.get_raw(item)
+        except KeyError:
+            await ctx.send("I could not find that item!")
+            return
+        if await bank.can_spend(ctx.author, cost):
+            old_bal = await bank.get_balance(ctx.author)
+            new_bal = old_bal - cost
+            await self.config.user(ctx.author).items.set_raw(item, value=cost)
+            await ctx.send("You bought a {0}!".format(item))
+            await bank.set_balance(ctx.author, new_bal)
 
     @commands.command(name="storeclear")
     @checks.is_owner()
