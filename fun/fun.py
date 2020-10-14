f1ab01b4a5d193ddfea78a549b92f0d1d18a5612
Jojo <jojohelp39@gmail.com>
2020-10-14 13:49:42 -0400

    Maybe register_member and register_user are different?

--- fun/fun.py
+++ fun/fun.py
@@ -12,8 +12,8 @@ class Fun(commands.Cog):
             scone=15,
             doughnut=9
         )
-        self.config.register_member(
-            stalk={}
+        self.config.register_user(
+            pets={}
         )
 
     def readable_dict(self, dictionary: dict):
@@ -35,12 +35,13 @@ class Fun(commands.Cog):
             await ctx.send("I could not find that item!")
             return
         if await bank.can_spend(ctx.author, cost):
-            # old_bal = await bank.get_balance(ctx.author)
-            # new_bal = old_bal - cost
-            # await self.config.user(ctx.author).items.set_raw(item, value=cost)
-            # await ctx.send("You bought a {0}!".format(item))
-            # await bank.set_balance(ctx.author, new_bal)
-            await self.config.user(ctx.author).stalk.set_raw(item, value={"Cost": cost})
+            await self.config.user(ctx.author).pets.set_raw(
+                item, value={"cost": cost, "hunger points": 0}
+            )
+            # await ctx.send("You have purchased a {0} and called it {1}".format(pet_type, pet_name))
+            # await self.update_balance(ctx.author, cost)
+        else:
+            await ctx.send("You do not have enough money to buy that pet!")
 
     @commands.command(name="storeclear")
     @checks.is_owner()
