52f31690892fe9d95d7413827ce1c46a17c38865
Jojo <jojohelp39@gmail.com>
2020-10-20 21:14:50 -0400

    ToDo init

---
+++ todo/todo.py
@@ -0,0 +1,58 @@
+from redbot.core import commands, checks, Config
+import discord
+
+
+class ToDo(commands.Cog):
+    """A simple todo list for discord"""
+    _default_global_settings = {
+        "DM": True
+    }
+    _default_member = {
+        "ToDo": []
+    }
+    default_user = _default_member
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.config = Config.get_conf(
+            self, 19924714009, force_registration=True)
+        self.config.register_guild(**self._default_global_settings)
+        self.config.register_member(**self._default_member)
+        self.config.register_user(**self.default_user)
+
+    async def whisper(self, ctx, user: discord.Member, msg: str = ""):
+        try:
+            await user.send(msg)
+        except discord.Forbidden:
+            return await ctx.send("I cannot dm that user.")
+
+    @commands.group()
+    @checks.admin_or_permissions(manage_server=True)
+    async def todoset(self, ctx):
+        """The base settings command for the ToDo cog"""
+
+    @todoset.command()
+    @checks.admin_or_permissions(manage_server=True)
+    async def dm(self, ctx, toggle: bool = True):
+        """Toggles the dm setting
+        will default to True if not specified"""
+        await self.config.guild(ctx.guild).DM.set_raw(value=toggle)
+        await ctx.send("Set the dm status to `{}`".format(toggle))
+
+    @commands.group()
+    async def todo(self, ctx):
+        """Base ToDo command"""
+
+    @todo.command()
+    async def add(self, ctx, title, *, description):
+        """Add a ToDo to your list"""
+        await self.config.user(ctx.author).ToDo.set_raw(title, value=description)
+        await ctx.send("Added `{0}` to your ToDo list under the title `{1}`.".format(description, title))
+
+    @todo.command(name="list")
+    async def _list(self, ctx):
+        todo_list = await self.config.user(ctx.author).ToDo.get_raw()
+        if await self.config.guild(ctx.guild()).DM is True:
+            await self.whisper(ctx, user=ctx.author, msg=todo_list)
+        else:
+            await ctx.send(todo_list)
