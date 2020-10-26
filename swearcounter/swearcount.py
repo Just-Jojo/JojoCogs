927f815057eabdfee436d8c58b1b7dff60c58030
Just-Jojo <jojohelp39@gmail.com>
2020-10-26 14:46:34 -0400

    okie..

--- swearcounter/swearcount.py
+++ swearcounter/swearcount.py
@@ -27,5 +27,5 @@ class SwearCount(commands.Cog):
 
     @commands.command()
     async def swearcount(self, ctx):
-        leaderboard = await self.config.get_raw()
+        leaderboard = await self.config.get_raw("swearcount")
         await ctx.send(leaderboard)
