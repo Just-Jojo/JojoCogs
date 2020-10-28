1ea1efc56dcf44f603fc556393ae42a2a0898287
Just-Jojo <jojohelp39@gmail.com>
2020-10-27 23:13:24 -0400

    Forgot to update the debug

--- brownies/brownie.py
+++ brownies/brownie.py
@@ -222,7 +222,7 @@ class Brownie(commands.Cog):
             msg = "I couldn't find anyone with brownie points"
             return msg
         players = await self.config.guild(user.guild).get_raw("Players")
-        print(players)
+        print(players.keys())
         if user not in players.keys():
             return "I could not find that user"
 
