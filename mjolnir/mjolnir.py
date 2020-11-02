418ffc69d74478f3b5c616f31f3df1d0389aaa79
Just-Jojo <jojohelp39@gmail.com>
2020-11-02 18:26:23 -0500

    Drop rates

--- mjolnir/mjolnir.py
+++ mjolnir/mjolnir.py
@@ -2,7 +2,6 @@ from redbot.core import commands, Config
 import discord
 import random
 import logging
-from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
 
 log = logging.getLogger('red.jojo.mjolnir')
 
@@ -11,22 +10,28 @@ class Mjolnir(commands.Cog):
     default_user = {
         "times_lifted": 0
     }
+    default_guild = {
+        "drop_rate": 100
+    }
 
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(self, 15034742, force_registration=True)
         self.config.register_user(**self.default_user)
+        self.config.register_guild(**self.default_guild)
 
     @commands.command()
     @commands.cooldown(1, 300, commands.BucketType.user)
     async def trylift(self, ctx):
+        """Try to lift Thor's mighty hammer"""
         trylift_error = [
             "You tried your best but still lost. Fear not! There is still hope!",
             "If you wish for the power of a god, you better be worthy of it",
             "No luck just yet, try again!",
             "You'll get it! Just keep trying! I believe in you"
         ]
-        trylift_result = random.randint(1, 300)
+        rate = await self.config.guild(ctx.guild).get_raw("drop_rate")
+        trylift_result = random.randint(1, rate)
         if trylift_result == 300:
             old = await self.config.user(ctx.author).get_raw('times_lifted')
             msg = "The sky opens up and a bolt of lightning strikes the ground\nYou are worthy. Hail, son of Odin"
@@ -36,6 +41,14 @@ class Mjolnir(commands.Cog):
         else:
             msg = random.choice(trylift_error)
         await ctx.send(msg)
+
+    @commands.command()
+    @commands.mod_or_permissions(manage_guild=True)
+    async def rates(self, ctx, number: int):
+        """Adjust the chance for lifting Mjolnir"""
+
+        await self.config.guild(ctx.guild).set_raw("drop_rate", value=number)
+        await ctx.send("The chance of lifting Mjolnir is now `{}`".format(number))
 
     @commands.command()
     async def liftedboard(self, ctx):
