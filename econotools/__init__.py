980f7bef5288d7b802dd779faa023cd409ad3166
Just-Jojo <jojohelp39@gmail.com>
2020-11-03 15:22:41 -0500

    I'm going to remove some cogs as I feel as if I do not wish to continue working on those type of cogs :D

--- econotools/__init__.py
+++
@@ -1,7 +0,0 @@
-from .economy_tools import EconomyTools
-# from .jojo_eco import JojoEconomy
-
-
-def setup(bot):
-    bot.add_cog(EconomyTools(bot))
-    # bot.add_cog(JojoEconomy(bot))
--- jojoeco/__init__.py
+++
@@ -1,4 +0,0 @@
-from .jojo_eco import JojoEconomy
-
-def setup(bot):
-    bot.add_cog(JojoEconomy(bot))
