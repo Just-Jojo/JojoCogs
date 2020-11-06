e121a88c2d19d76c811177c2296608e0c3815858
Just-Jojo <jojohelp39@gmail.com>
2020-11-06 13:30:52 -0500

    Rewrote the todo cog :D

--- todo/todo.py
+++ todo/todo.py
@@ -1,103 +1,91 @@
 from redbot.core import commands, checks, Config
+from redbot.core.utils import menus
 import discord
 import logging
+import asyncio
 
 log = logging.getLogger("red.jojo.todo")
 
 
 class ToDo(commands.Cog):
     """A simple todo list for discord"""
-    _default_global_settings = {
-        "DM": True
+
+    default_user = {
+        "ToDo": "Write a ToDo list"
     }
 
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(
             self, 19924714009, force_registration=True)
-        self.config.register_guild(**self._default_global_settings)
         self.config.register_user(
-            todo={
-                "ToDo": "Create a ToDo reminder"
-            }
+            **self.default_user
         )
 
     @commands.group()
-    @commands.guild_only()
-    @checks.guildowner()
-    async def todoset(self, ctx):
-        """The base settings command for the ToDo cog"""
-
-    @todoset.command()
-    async def dm(self, ctx, toggle: bool = None):
-        """Toggles the dm setting
-        will default to True if not specified"""
-        if toggle is None:
-            return await ctx.send("I can't set it to `None`!")
-        old = await self.check_dm(ctx)
-        if toggle == old:
-            msg = "That is the same setting!\nSilly goose"
-        else:
-            await self.config.guild(ctx.guild).set_raw("DM", value=toggle)
-            msg = "Set the DM status to `{}`\n\n`True meaning I will dm the user, False meaning I will send it to the server`".format(
-                toggle)
-        await ctx.send(msg)
-
-    @commands.group()
     async def todo(self, ctx):
-        """Base ToDo command"""
+        """Base To do reminder command"""
 
     @todo.command()
-    async def add(self, ctx, title, *, description):
-        """Add a ToDo to your list"""
-        await self.config.user(ctx.author).todo.set_raw(title, value=description)
-        await ctx.send("Added `{0}` to your ToDo list under the title `{1}`.".format(description, title))
+    async def add(self, ctx, title: str, *, description: str):
+        """Add a to do reminder"""
+        await self.config.user(ctx.author).set_raw(title, value=description)
+        await ctx.send("I have added the reminder under the name `{}`".format(title))
 
-    @todo.command(aliases=["del", ])
-    async def remove(self, ctx, *, reminder: str = None):
-        """Remove a ToDo reminder"""
-        toogle = False if isinstance(ctx.channel, discord.DMChannel) else await self.check_dm(ctx)
-        if reminder is None:
-            todos = await self.get_todos(ctx, ctx.author)
-            if toogle is False:
-                return await ctx.send(todos)
+    @todo.command(aliases=["del", "delete"])
+    async def remove(self, ctx, todo: str):
+        """Remove a to do reminder
+        Note this *is* case sensitive"""
+        try:
+            await self.config.user(ctx.author).clear_raw(todo)
+        except KeyError:
+            return await ctx.send("I could not find that ToDo reminder. Please use `{}todo list`".format(
+                ctx.clean_prefix
+            ))
+        await ctx.send("Removed `{}` from your to do list!".format(todo))
+
+    @todo.command(name="list")  # Fuck you, reserved keywords >:|
+    async def todo_list(self, ctx):
+        """List your ToDo reminders"""
+        todos = await self.config.user(ctx.author).get_raw()
+        await self.page_logic(ctx, todos)
+
+    async def page_logic(self, ctx: commands.Context, object: dict) -> None:
+        count = 0
+        embeds: list
+        embed = self.create(
+            ctx, title=f"{ctx.author}'s ToDo list", color=ctx.author.color,
+            footer=f"{ctx.author.name}'s ToDos'"
+        )
+        for key, value in object.items():
+            if count == 5:
+                embed.add_field(name=key, value=value, inline=True)
+                embeds.append(embed)
+                count = 0
+                embed = self.create(
+                    ctx, title=f"{ctx.author}'s ToDo list", color=ctx.author.color,
+                    footer=f"{ctx.author.name}'s ToDos'"
+                )
             else:
-                try:
-                    return await ctx.author.send(todos)
-                except discord.Forbidden:
-                    return await ctx.send("I could not send you the message!\n{}".format(todos))
-
-        await self.config.user(ctx.author).todo.clear_raw(reminder)
-        await ctx.send("I have removed `{}` from your ToDo list".format(reminder))
-
-    @todo.command(name="list")
-    async def _list(self, ctx):
-        todo_list = self.readable_dict(await self.config.user(ctx.author).todo.get_raw())
-        if isinstance(ctx.channel, discord.abc.GuildChannel):
-            toggle = await self.check_dm(ctx)
+                embed.add_field(name=key, value=value, inline=True)
+                count += 1
         else:
-            toggle = False
-        if toggle is True:
-            try:
-                return await ctx.author.send(todo_list)
-            except discord.Forbidden:
-                return await ctx.send("I could not dm you the message!")
-        await ctx.send(todo_list)
+            embeds.append(embed)
 
-    def readable_dict(self, dictionary: dict, numbered: bool = False) -> str:
-        readable = []
-        for num, (key, item) in enumerate(dictionary.items()):
-            if numbered is True:
-                string_version = "{0}. {1}: {2}".format(num, key, item)
-            else:
-                string_version = "{0}: {1}".format(key, item)
-            readable.append(string_version)
-        return "\n".join(readable)
+        msg = ctx.send(embeds[0])
+        controls = menus.DEFAULT_CONTROLS if len(embeds) > 1 else {
+            "\N{CROSS MARK}": menus.close_menu
+        }
+        asyncio.create_task(menus.menu(ctx, embeds, controls, msg, timeout=15))
+        menus.start_adding_reactions(msg, controls.keys())
 
-    async def check_dm(self, ctx) -> bool:
-        toogle: bool = await self.config.guild(ctx.guild).get_raw("DM")
-        return toogle
-
-    async def get_todos(self, ctx: commands.Context, author: discord.Member) -> str:
-        todos = await self.config.user(author).todo.get_raw()
-        return self.readable_dict(todos, True)
+    def create(
+        self, ctx, title: str = "", color: discord.Colour = None, footer: str = None, footer_url: str = None
+    ) -> discord.Embed:
+        data = discord.Embed(title=title, color=color)
+        if footer is None:
+            footer = ctx.author.name
+        if footer_url is None:
+            footer_url = ctx.author.avatar_url
+        data.set_footer(text=footer, icon_url=footer_url)
+        return data
