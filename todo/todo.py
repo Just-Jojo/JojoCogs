7cdef02db79da095bacb14ffe8b161dc90efaa1f
Just Jojo <justjojoGH@gmail.com>
2021-01-07 12:53:18 -0500

    Fixed issues with removing and adding todo reminders

--- todo/todo.py
+++ todo/todo.py
@@ -4,7 +4,7 @@ from typing import Literal
 
 import discord
 from redbot.core import Config, checks, commands
-from redbot.core.utils.chat_formatting import pagify
+from redbot.core.utils.chat_formatting import pagify, humanize_list as hl
 from redbot.core.utils import menus
 
 log = logging.getLogger("red.jojo.todo")
@@ -38,33 +38,44 @@ class ToDo(commands.Cog):
 
         Example:
         `[p]todo add Walk the dog soon`"""
-        await self.config.user(ctx.author).set_raw("todos", value=todo)
-        await ctx.send("Added the reminder `{}`".format(todo))
+        todos = await self.config.user(ctx.author).todos()
+        todos.append(todo)
+        await self.config.user(ctx.author).todos.set(todos)
+        await ctx.send("Added that todo reminder!")
 
     @todo.command(aliases=["del", "delete"])
-    async def remove(self, ctx, todo: str):
+    async def remove(self, ctx, todo: int = None):
         """Remove a to do reminder
 
         Example:
-        `[p]todo remove|del|delete ToDo`
-
-        Note this *is* case sensitive"""
+        `[p]todo del <number>`"""
+        todos = await self.config.user(ctx.author).todos()
+        if len(todos) < 1:
+            await ctx.send(f"You don't have any todos yet!\nUse `{ctx.clean_prefix}todo add <todo>` to add one!")
+            return
+        if todo is None:  # can't use `not todo` since `0` is falsy
+            sending = []
+            for index, item in enumerate(todos):
+                sending.append(f"{index} {item}")
+            await self.page_logic(ctx, sending)
+            return
         try:
-            await self.config.user(ctx.author).clear_raw("todos", todo)
-        except KeyError:
-            return await ctx.send("I could not find that ToDo reminder. Please use `{}todo list`".format(
-                ctx.clean_prefix
-            ))
-        await ctx.send("Removed `{}` from your to do list!".format(todo))
+            del todos[todo]
+        except IndexError:
+            await ctx.send("That was an invalid todo index!")
+        else:
+            await ctx.send("Removed that todo!")
+            await self.config.user(ctx.author).todos.set(todos)
 
     @todo.command(name="list")  # Fuck you, reserved keywords >:|
     async def todo_list(self, ctx):
         """List your ToDo reminders"""
-        todos = await self.config.user(ctx.author).get_raw("todos")
+        todos = await self.config.user(ctx.author).todos()
+        log.info(type(todos))
         if len(todos) >= 1:
             await self.page_logic(ctx, todos)
         else:
-            await ctx.send(f"You don't have any ToDo reminders!\nYou can add one using `{ctx.clean_prefix}todo add <name> <ToDO>`")
+            await ctx.send(f"You don't have any ToDo reminders!\nYou can add one using `{ctx.clean_prefix}todo add <name> <ToDo>`")
 
     async def page_logic(self, ctx: commands.Context, object: list) -> None:
         embeds = []
@@ -72,12 +83,15 @@ class ToDo(commands.Cog):
             "title": "{}'s ToDos".format(ctx.author.display_name),
             "color": ctx.author.color
         }
-        paged = pagify(object, page_length=500)
+        object = [f"`{x}`" for x in object]
+        paged = pagify(", ".join(object), page_length=500)  # hl(object)
         for i in paged:
             embed = discord.Embed(**values)
             embed.set_author(name=ctx.author.name,
                              icon_url=ctx.author.avatar_url)
             embed.add_field(name="ToDo's", value=i)
+            embed.set_footer(
+                text=f"Use `{ctx.clean_prefix}todo add <todo>` to add a todo | `{ctx.clean_prefix}todo del <index>` to remove a todo")
             embeds.append(embed)
 
         msg = await ctx.send(embed=embeds[0])
