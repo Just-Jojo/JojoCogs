b55b87fbdd8cc8e73d1ba0366e07394907193617
Just Jojo <justjojoGH@gmail.com>
2021-02-18 23:17:36 -0500

    [Ping] V0.1.3 extra customizable options

--- ping/__init__.py
+++ ping/__init__.py
@@ -1,18 +1,18 @@
 from redbot.core.bot import Red
 from redbot.core.commands import command
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 
 @command()
-async def pingset(ctx):
+async def pingset(ctx, mention: bool = False, response: str = "Pong."):
     """Set the ping command to use replies"""
     bot = ctx.bot
 
     @command()
     async def ping(ctx):
         """Pong"""
-        await ctx.reply("Pong.", mention_author=False)
+        await ctx.reply(response, mention_author=mention)
 
     bot.remove_command("ping")
     bot.add_command(ping)
