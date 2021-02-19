0928b8b09807bda0dde54ce336b60353816ef69b
Just Jojo <justjojoGH@gmail.com>
2021-02-18 23:18:36 -0500

    [Ping] Optional]

--- ping/__init__.py
+++ ping/__init__.py
@@ -1,11 +1,13 @@
 from redbot.core.bot import Red
 from redbot.core.commands import command
 
+from typing import Optional
+
 __version__ = "0.1.3"
 
 
 @command()
-async def pingset(ctx, mention: bool = False, response: str = "Pong."):
+async def pingset(ctx, mention: Optional[bool] = False, response: str = "Pong."):
     """Set the ping command to use replies"""
     bot = ctx.bot
 
