c57670467512ff7bcb6a260ac7cfba8cb9297aeb
Just Jojo <fieldsjoseph101@gmail.com>
2021-02-23 13:14:50 -0500

    Uh, okay?

--- ping/__init__.py
+++ ping/__init__.py
@@ -7,16 +7,9 @@ __version__ = "0.1.3"
 
 
 @command()
-<<<<<<< HEAD
 async def ping(ctx, response: str, mention: bool = False):
     """Pong"""
     await ctx.reply(response, mention_author=mention)
-=======
-@is_owner()
-async def pingset(ctx, mention: Optional[bool] = False, *, response: str = "Pong."):
-    """Set the ping command to use replies"""
-    bot = ctx.bot
->>>>>>> 27427dcea9c03cf87c2c54841fdcc6eb40f59f7c
 
 
 def setup(bot: Red):
