abc4f37d5b41dd6d5c6288c5db2dc11e4ca54fa3
Just Jojo <justjojogh@gmail.com>
2021-03-20 14:16:11 -0400

    [Ping] Add the old ping command back on unload

--- ping/__init__.py
+++ ping/__init__.py
@@ -1,15 +1,26 @@
 from redbot.core.bot import Red
-from redbot.core.commands import command
+from redbot.core.commands import command, Command
 
 __version__ = "0.1.4"
 
+OLD_PING: Command = None
 
-@command()
+
+@command(hidden=True)
 async def ping(ctx):
     """Pong"""
     await ctx.reply(content="Pong.", mention_author=False)
 
 
 def setup(bot: Red):
-    bot.remove_command("ping")
+    global OLD_PING
+    OLD_PING = bot.remove_command("ping")
+
     bot.add_command(ping)
+
+
+def teardown(bot: Red):
+    bot.remove_command("ping")
+    global OLD_PING
+    if OLD_PING:
+        bot.add_command(OLD_PING)
