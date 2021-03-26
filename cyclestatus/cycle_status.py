e509705025fe757caf9bbefddb4567fef1a16821
Just Jojo <justjojogh@gmail.com>
2021-03-26 16:49:53 -0400

    [CycleStatus] v1.0.0 Rewrite of CycleStatus

--- cyclestatus/cycle_status.py
+++ cyclestatus/cycle_status.py
@@ -23,138 +23,138 @@ SOFTWARE.
 """
 
 import logging
-from itertools import cycle
 
 import discord
 from discord.ext import tasks
-from redbot.core import Config, commands
+
+from redbot.core import commands, Config
 from redbot.core.bot import Red
-from redbot.core.utils.chat_formatting import box, humanize_list
+from redbot.core.utils.chat_formatting import pagify
+from itertools import cycle
+
+from .menus import Pages, Menu
+
+from typing import List
 
 log = logging.getLogger("red.JojoCogs.cyclestatus")
+_config_structure = {
+    "global": {
+        "statuses": [],
+        "use_help": True,
+        "next_iter": 0,
+    },
+}
 
-_config_structure = {"statuses": [], "type": "game", "help": True}
+
+def positive_int(arg: str) -> int:
+    try:
+        ret = int(arg)
+    except ValueError:
+        raise commands.BadArgument(message=f"{arg} is not an int")
+    if ret < 1:
+        raise commands.BadArgument(message=f"'{arg}' is not positive")
+    return ret
 
 
 class CycleStatus(commands.Cog):
-    __version__ = "0.1.1"
+    """Automatically change the status of your bot every minute"""
+
+    __version__ = "1.0.0"
     __author__ = ["Jojo#7791"]
 
     def __init__(self, bot: Red):
         self.bot = bot
-        self.config = Config.get_conf(self, 3213546583213654, True)
-        self.config.register_global(**_config_structure)
-        self.removed = False
+        self.config = Config.get_conf(self, 115849, True)
+        self.config.register_global(**_config_structure["global"])
 
     async def init(self):
-        await self._update_status()
-        self.update_status.start()
+        await self.bot.wait_until_red_ready()
+        self.main_task.start()
 
     def cog_unload(self):
-        self.update_status.cancel()
+        self.main_task.cancel()
 
-    async def _update_status(self):
-        stats = await self.config.statuses()
-        if not len(stats):
-            self.cy = None
-            if self.removed is False:
-                await self.bot.change_presence()
-                self.removed = True
-        else:
-            self.cy = cycle(stats)
-            if self.removed:
-                self.removed = False
-        await self.update_status()
+    def format_help_for_context(self, ctx: commands.Context):
+        return (
+            f"{super().format_help_for_context(ctx)}"
+            f"\nVersion `{self.__version__}`"
+            f"\nAuthor(s) `{', '.join(self.__author__)}`"
+        )
 
     @commands.group()
+    @commands.is_owner()
     async def status(self, ctx):
-        """Base command for Status options"""
+        """Commands working with the status"""
+        pass
 
-    @status.group(name="set", invoke_without_command=True, usage="<activity type>")
-    async def status_set(self, ctx, ac_type: str):
-        """Set the type of the status activity!"""
-        ac_type = ac_type.lower()
-        if ac_type not in ("game", "watching", "listening"):
-            return await ctx.send("That wasn't a valid type!")
+    @status.command(name="usehelp")
+    async def status_set(self, ctx, toggle: bool = None):
+        """Change whether the status should have ` | [p]help`"""
+        if toggle is None:
+            msg = f"Added help is {'enabled' if await self.config.use_help() else 'disabled'}"
+            return await ctx.send(msg)
+        await self.config.use_help.set(toggle)
         await ctx.tick()
-        await self.config.type.set(ac_type)
-
-    @status_set.command(name="help")
-    async def status_set_help(self, ctx, toggle: bool):
-        """Appends `[p]help` to the end of the status"""
-        if (s := await self.config.help()) == toggle:
-            await ctx.send(f"Help was already {'enabled' if toggle else 'disabled'}")
-        else:
-            await ctx.tick()
-            await self.config.help.set(toggle)
 
     @status.command(name="add")
     async def status_add(self, ctx, *, status: str):
         """Add a status to the list"""
-        async with self.config.statuses() as stat:
-            stat.append(status)
+        async with self.config.statuses() as s:
+            s.append(status)
         await ctx.tick()
-        await self.update_status()
 
-    @status.command(name="remove", aliases=["del", "delete"])
-    async def status_remove(self, ctx, *indexes: int):
+    @status.command(name="remove", aliases=["del", "rm", "delete"])
+    async def status_remove(self, ctx, num: positive_int = None):
         """Remove a status from the list"""
-        if not len(indexes):
-            msg = await self.config.statuses()
-            msg = "\n".join([f"{num}. {x}" for num, x in enumerate(msg, 1)])
-            return await ctx.send(box(msg, "md"))
-        (indexes := [i - 1 for i in indexes]).sort(reverse=True)
-        async with self.config.statuses() as stat:
-            for index in indexes:
-                try:
-                    stat.pop(index)
-                except IndexError:
-                    pass
+        if num is None:
+            return await ctx.invoke(self.status_list)
+        num -= 1
+        async with self.config.statuses() as sts:
+            if num >= len(sts):
+                return await ctx.send("You don't have that many statuses, silly")
+            sts.pop(num)
         await ctx.tick()
-        await self._update_status()
 
     @status.command(name="list")
     async def status_list(self, ctx):
-        """List the statuses your bot has"""
-        msg = await self.config.statuses()
-        if not msg:
-            return await ctx.send("You don't have any statuses!")
-        msg = "\n".join(f"{num}. {x}" for num, x in enumerate(msg, 1))
-        await ctx.send(box(msg, "md"))
+        """List the available statuses"""
+        if not (status := await self.config.statuses()):
+            return await ctx.send("There are no statuses")
+        await self._show_statuses(ctx=ctx, statuses=status)
 
-    @tasks.loop(minutes=5)
-    async def update_status(self):
-        if self.cy is None:
+    @tasks.loop(minutes=1)
+    async def main_task(self):
+        statuses = await self.config.statuses()
+        if not statuses:
             return
-
-        add_help = await self.config.help()
-        game_type = await self.config.type()
-        msg = next(self.cy)
-        if add_help:
-            prefix = await self.bot.get_valid_prefixes()
-            for p in prefix:
-                if p not in (
-                    f"<@!{self.bot.user.id}> ",
-                    f"<@{self.bot.user.id}> ",
-                    f"<@!{self.bot.user.id}>",
-                    f"<@{self.bot.user.id}>",
-                ):
-                    prefix = p
-                    break
-            if isinstance(prefix, list):
+        msg = statuses[(nl := await self.config.next_iter())]
+        if await self.config.use_help():
+            prefix = (await self.bot.get_valid_prefixes())[0]
+            if prefix == str(self.bot.user.mention):
                 prefix = f"@{self.bot.user.name}"
             msg += f" | {prefix}help"
-        if game_type == "game":
-            ac = discord.Game(name=msg)
-        elif game_type == "listening":
-            ac = discord.Activity(name=msg, type=discord.ActivityType.listening)
+        game = discord.Activity(name=msg, type=discord.ActivityType.listening)
+        await self.bot.change_presence(activity=game)
+        if len(statuses) - 1 == nl:
+            nl = 0
         else:
-            ac = discord.Activity(name=msg, type=discord.ActivityType.watching)
-        await self.bot.change_presence(activity=ac)
+            nl += 1
+        await self.config.next_iter.set(nl)
 
-    async def cog_check(self, ctx: commands.Context):
-        return await self.bot.is_owner(ctx.author)
+    async def _num_lists(self, data: List[str]) -> List[str]:
+        """|coro|
 
-    async def red_delete_data_for_user(self, *, requester, user_id):
+        Return a list of numbered items
+        """
+        return [f"{num}. {d}" for num, d in enumerate(data, 1)]
+
+    async def _show_statuses(self, ctx: commands.Context, statuses: List[str]) -> None:
+        source = Pages(
+            list(pagify("\n".join(await self._num_lists(statuses)), page_length=400)),
+            title="Statuses",
+        )
+        await Menu(source=source).start(ctx, channel=ctx.channel)
+
+    async def red_delete_data_for_user(self, *, requester: str, user_id: int) -> None:
         """Nothing to delete"""
         return
---
+++ cyclestatus/menus.py
@@ -0,0 +1,148 @@
+"""
+MIT License
+
+Copyright (c) 2020-2021 Jojo#7711
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from typing import List, Union
+
+import discord
+
+from redbot.core import commands
+from redbot.core.bot import Red
+from redbot.core.utils.chat_formatting import box
+from redbot.vendored.discord.ext import menus
+
+
+__all__ = ["Pages", "Menu"]
+
+
+class Pages(menus.ListPageSource):
+    """Custom list page source for the menu"""
+
+    def __init__(self, entries: Union[List[discord.Embed], List[str]], title: str):
+        super().__init__(entries, per_page=1)
+        self._title = title
+
+    def is_paginating(self) -> bool:
+        return True
+
+    async def format_page(self, menu: "Menu", page: str) -> Union[discord.Embed, str]:
+        ctx: commands.Context = menu.ctx
+        footnote = f"Page {menu.current_page + 1}/{self.get_max_pages()}"
+        if await ctx.embed_requested():
+            embed = discord.Embed(
+                title=self._title,
+                colour=await ctx.embed_color(),
+                description=box(page, "md"),
+            )
+            embed.set_footer(text=footnote)
+            return embed
+        return f"{self._title}\n{box(page, 'md')}\n{footnote}"
+
+
+class Menu(menus.MenuPages, inherit_buttons=False):
+    message: discord.Message
+    ctx: commands.Context
+    _source: Pages
+
+    def __init__(
+        self,
+        source: Pages,
+        clear_reactions_after: bool = True,
+        delete_message_after: bool = False,
+        message: discord.Message = None,
+        timeout: float = 15.0,
+    ):
+        super().__init__(
+            source,
+            clear_reactions_after=clear_reactions_after,
+            delete_message_after=delete_message_after,
+            message=message,
+            timeout=timeout,
+        )
+        self._page_start = 0
+
+    async def send_initial_message(
+        self, ctx: commands.Context, channel: discord.TextChannel
+    ) -> discord.Message:
+        self.current_page = self._page_start
+        page = await self._source.get_page(self.current_page)
+        kwargs = await self._get_kwargs_from_page(page)
+        return await channel.send(**kwargs)
+
+    async def show_checked_page(self, page_number: int):
+        max_pages = self._source.get_max_pages()
+        if max_pages is None or max_pages > page_number >= 0:
+            await self.show_page(page_number)
+        elif page_number > max_pages:
+            await self.show_page(0)
+        elif page_number < 0:
+            await self.show_page(max_pages - 1)
+
+    def _skip_single_arrows(self):
+        max_pages = self._source.get_max_pages()
+        if max_pages is None:
+            return True
+        return max_pages == 1
+
+    def _skip_double_triangle_buttons(self):
+        max_pages = self._source.get_max_pages()
+        if max_pages is None:
+            return True
+        return max_pages <= 4
+
+    @menus.button(
+        "\N{BLACK LEFT-POINTING DOUBLE TRIANGLE}",
+        position=menus.First(0),
+        skip_if=_skip_double_triangle_buttons,
+    )
+    async def go_to_first_page(self, payload):
+        await self.show_checked_page(0)
+
+    @menus.button(
+        "\N{BLACK RIGHT-POINTING DOUBLE TRIANGLE}",
+        position=menus.Last(1),
+        skip_if=_skip_double_triangle_buttons,
+    )
+    async def go_to_last_page(self, payload):
+        await self.show_checked_page(self._source.get_max_pages() - 1)
+
+    @menus.button(
+        "\N{LEFTWARDS BLACK ARROW}",
+        position=menus.First(1),
+        skip_if=_skip_single_arrows,
+    )
+    async def go_to_previous_page(self, payload):
+        await self.show_checked_page(self.current_page - 1)
+
+    @menus.button(
+        "\N{BLACK RIGHTWARDS ARROW}",
+        position=menus.Last(0),
+        skip_if=_skip_single_arrows,
+    )
+    async def go_to_next_page(self, payload):
+        await self.show_checked_page(self.current_page + 1)
+
+    @menus.button("\N{CROSS MARK}")
+    async def stop_pages(self, payload):
+        self.stop()
+        await self.message.delete()
