25836ca8ffbb26f9f659e3c9d9dd0f0b34247b5e
Just Jojo <justjojogh@gmail.com>
2021-04-16 15:16:49 -0400

    [Meta] Add a make file because I'm lazy

---
+++ make.bat
@@ -0,0 +1,21 @@
+@echo off
+
+if [%1] == [] goto help
+
+REM This allows us to expand variables at execution
+setlocal ENABLEDELAYEDEXPANSION
+
+goto %1
+
+:reformat
+isort --line-length 90 .
+black -l 90 .
+exit /B %ERRORLEVEL%
+
+:help
+echo Usage:
+echo   make ^<command^>
+echo.
+echo Commands:
+echo   reformat                   Reformat all .py files being tracked by git.
+REM I may or may not have ripped this off of kreusada's make file so... lol
