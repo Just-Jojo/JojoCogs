852f044309fbf87341f1e04ef2878f7b473665aa
Just-Jojo <justjojogh@gmail.com>
2021-05-13 16:18:18 -0400

    [Meta] 'Proper' __red_end_user_data_statement__ and type hints for __init__.py files

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,11 +1,13 @@
 import json
 import pathlib
 
-from .todo import ToDo
+from redbot.core.bot import Red
+
+from .todo import ToDo  # type:ignore[attr-defined] # Silly mypy
 
 with open(pathlib.Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
-def setup(bot):
+def setup(bot: Red):
     bot.add_cog(ToDo(bot))
