96fc44709a8b8d63c5fa1e864ae433156dbb0d31
Just-Jojo <justjojogh@gmail.com>
2021-06-19 05:40:24 -0400

    [UpdateUtils] Forgot end user data statement stuff

--- updateutils/__init__.py
+++ updateutils/__init__.py
@@ -1,10 +1,13 @@
 import json
 from pathlib import Path
+from redbot.core.bot import Red
 
 from .update import UpdateUtils
 
-# TODO Code for info.json stuff
+
+with open(Path(__file__).parent / "info.json", "r") as fp:
+    __red_end_user_data_statement__ = json.load(fp)
 
 
-def setup(bot):
+def setup(bot: Red):
     bot.add_cog(UpdateUtils(bot))
