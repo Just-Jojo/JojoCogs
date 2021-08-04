0e9093c6e4dbfceeb205d1d6a592edf74a6a8675
Just-Jojo <justjojogh@gmail.com>
2021-08-04 16:20:40 -0400

    [CmdLogger] V1.0.0 Initial commit
    This cog allows for users to log command usage with the basic logging using the logging module. Optionally, you can have a command be logged to a channel

---
+++ cmdlogger/converters.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2021 - Jojo#7791
+# Licensed under MIT
+
+from redbot.core import commands
+
+
+__all__ = ["CommandConverter", "NoneChannelConverter"]
+
+
+class CommandConverter(commands.Converter):
+    async def convert(self, ctx: commands.Context, arg: str):
+        ret = ctx.bot.get_command(arg)
+        if arg is None:
+            raise commands.BadArgument(f"'{arg}' is not a command")
+        return ret
+
+
+class NoneChannelConverter(commands.Converter):
+    async def convert(self, ctx: commands.Context, arg: str):
+        if arg == "None":
+            return None
+        return await commands.TextChannelConverter().convert(ctx, arg)
---
+++ cmdlogger/menus.py
@@ -0,0 +1,107 @@
+# Copyright (c) 2021 - Jojo#7791
+# Licensed under MIT
+
+import discord
+
+from redbot.core import commands
+from redbot.vendored.discord.ext.menus import MenuPages, ListPageSource, button, First, Last
+
+from contextlib import suppress
+
+
+__all__ = ["CmdMenu", "CmdPages"]
+
+class CmdPages(ListPageSource):
+    def __init__(self, data: list):
+        super().__init__(data, per_page=1)
+
+    async def format_page(self, menu: "CmdMenu", page: str):
+        ctx: commands.Context = menu.ctx
+        footer = f"Page {menu.current_page + 1}/{self.get_max_pages()}"
+
+        if await ctx.embed_requested():
+            embed = discord.Embed(
+                title="Cmd logging", description=page, colour=await ctx.embed_colour()
+            ).set_footer(text=footer)
+            return embed
+        return (
+            f"**Cmd logging**\n{page}\n{footer}"
+        )
+
+
+class CmdMenu(MenuPages):
+    ctx: commands.Context
+
+    @property
+    def source(self) -> CmdPages:
+        return self._source
+
+    def __init__(self, source: CmdPages):
+        super().__init__(source, clear_reactions_after=True)
+
+    def _skip_double_triangle_buttons(self):
+        max_pages = self._source.get_max_pages()
+        if max_pages is None:
+            return True
+        return max_pages <= 4
+
+    async def send_initial_message(self, ctx, channel):
+        page = await self.source.get_page(0)
+        kwargs = await self._get_kwargs_from_page(page)
+        return await channel.send(**kwargs)
+
+    def _skip_single_triangle_buttons(self):
+        max_pages = self._source.get_max_pages()
+        if max_pages is None:
+            return True
+        return max_pages == 1
+
+    async def show_checked_page(self, page_number: int):
+        max_pages = self.source.get_max_pages()
+        try:
+            if max_pages is None or max_pages > page_number >= 0:
+                await self.show_page(page_number)
+            elif max_pages <= page_number:
+                await self.show_page(0)
+            elif page_number < 0:
+                await self.show_page(max_pages - 1)
+        except IndexError:
+            pass
+
+    @button(
+        "\N{BLACK LEFT-POINTING DOUBLE TRIANGLE}\N{VARIATION SELECTOR-16}",
+        skip_if=_skip_double_triangle_buttons,
+        position=First(0),
+    )
+    async def go_to_first_page(self, payload):
+        await self.show_page(0)
+
+    @button(
+        "\N{LEFTWARDS BLACK ARROW}\N{VARIATION SELECTOR-16}",
+        skip_if=_skip_single_triangle_buttons,
+        position=First(1),
+    )
+    async def go_to_previous_page(self, payload):
+        await self.show_checked_page(self.current_page - 1)
+
+    @button(
+        "\N{BLACK RIGHTWARDS ARROW}\N{VARIATION SELECTOR-16}",
+        skip_if=_skip_single_triangle_buttons,
+        position=Last(0)
+    )
+    async def go_to_next_page(self, payload):
+        await self.show_checked_page(self.current_page + 1)
+
+    @button(
+        "\N{BLACK RIGHT-POINTING DOUBLE TRIANGLE}\N{VARIATION SELECTOR-16}",
+        skip_if=_skip_double_triangle_buttons,
+        position=Last(1)
+    )
+    async def go_to_last_page(self, payload):
+        await self.show_checked_page(self.source.get_max_pages() - 1)
+
+    @button("\N{CROSS MARK}")
+    async def stop_pages(self, payload):
+        self.stop()
+        with suppress(discord.Forbidden, discord.NotFound):
+            await self.message.delete()
