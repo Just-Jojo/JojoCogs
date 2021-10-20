5275fe654e4644c1d135e0706546887ee7d98768
Just-Jojo <justjojogh@gmail.com>
2021-10-19 22:25:09 -0400

    [AdvancedBlacklist] Forgot to update the init file

--- advancedblacklist/__init__.py
+++ advancedblacklist/__init__.py
@@ -9,6 +9,5 @@ with open(pathlib.Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
-async def setup(bot: Red):
-    cog = await AdvancedBlacklist.init(bot)
-    bot.add_cog(cog)
+def setup(bot: Red):
+    bot.add_cog(AdvancedBlacklist(bot))
