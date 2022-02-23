0021883222e1f4c41cac8c04299dcba541372802
Just-Jojo <justjojogh@gmail.com>
2022-02-23 09:38:02 -0500

    [ErrorBlacklist] More typing stuff

--- errorblacklist/api.py
+++ errorblacklist/api.py
@@ -1,27 +1,32 @@
 # Copyright (c) 2021 - Jojo#7791
 # Licensed under MIT
 
+from __future__ import annotations
+
 from typing import Iterable, Optional, Set, Union
 
 from discord import Guild, Member, Role, User
 from redbot import VersionInfo, version_info
 from redbot.core.bot import Red
 
+__all__ = ["add_to_blacklist", "in_blacklist", "get_blacklist"]
+
+
 if version_info.dev_release is None and version_info >= VersionInfo.from_str("3.4.13"):
     add_to_blacklist = Red.add_to_blacklist
     get_blacklist = Red.get_blacklist
 else:
-    UserOrRole = Union[User, Member, Role, int]
+    UserOrRole = Union[int, User, Member, Role]
 
     async def add_to_blacklist(
-        bot: Red, users_or_roles: Iterable[UserOrRole], *, guild: Optional[Guild] = None
-    ) -> None:
+        self: Red, users_or_roles: Iterable[UserOrRole], *, guild: Optional[Guild] = None
+    ):
         to_add = {getattr(x, "id", x) for x in users_or_roles}
-        await bot._whiteblacklist_cache.add_to_blacklist(guild, to_add)
-        bot.dispatch("blacklist_add", to_add, guild)
+        await self._whiteblacklist_cache.add_to_blacklist(guild, to_add)
+        self.dispatch("blacklist_add", to_add, guild)
 
-    async def get_blacklist(bot: Red, guild: Optional[Guild] = None) -> Set[int]:
-        return await bot._whiteblacklist_cache.get_blacklist(guild)
+    async def get_blacklist(self: Red, guild: Optional[Guild] = None) -> Set[int]:
+        return await self._whiteblacklist_cache.get_blacklist(guild)
 
 
 async def in_blacklist(
